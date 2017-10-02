4f8ae247bd9a6ab3be0723e7db5e282ad694268a
Fauricio <fauriciocr@gmail.com>
2017-10-02 15:50:18 -0600

    Creacion de gets y sets

--- Proyecto1/Proyecto1/Modelo/Comentario.cs
+++ Proyecto1/Proyecto1/Modelo/Comentario.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Proyecto1.Modelo
 {
-    class Comentario
+    public class Comentario
     {
+        private string comentario;
+        public Comentario() { }
+
+        public Comentario(string coment) {
+            this.comentario = coment;
+        }
+
+        public void setComentario(string coment)
+        {
+            this.comentario = coment;
+        }
+
+        public string getComentario()
+        {
+            return comentario;
+        }
+
     }
 }
