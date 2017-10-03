e3122307f706e99865fbcf15e9916bc905d9a308
AdanMora <ajmorafallas@gmail.com>
2017-10-03 10:03:22 -0600

    Espero que funciones

--- Proyecto1/Proyecto1/Controlador/DB_DAO.cs
+++ Proyecto1/Proyecto1/Controlador/DB_DAO.cs
@@ -6,15 +6,7 @@ using System.Threading.Tasks;
 
 namespace Proyecto1.Controlador
 {
-    public class DB_DAO
+    class DB_DAO
     {
-        private static Proyecto1DB db = new Proyecto1DB();
-
-        public DB_DAO() {
-            Miembro m = new Miembro();
-
-        }
-
-
     }
 }
