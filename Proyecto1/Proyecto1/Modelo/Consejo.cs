aada2d01272f6acae749b06ea11353f20f623303
Fauricio <fauriciocr@gmail.com>
2017-10-03 10:31:46 -0600

    Creación de métodos

--- Proyecto1/Proyecto1/Modelo/Consejo.cs
+++ Proyecto1/Proyecto1/Modelo/Consejo.cs
@@ -5,14 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
 
-namespace Proyecto1.NewFolder1
+
+namespace Proyecto1.Modelo
 {
     class Consejo
     {
         private ArrayList miembros = new ArrayList();
         private ArrayList sesiones = new ArrayList();
         private ArrayList solicitudes = new ArrayList();
+        
 
         public Consejo() { }
+
+        public void agregarMiembro(string nombre,string correo1,string correo2,char tipo)
+        {
+            Miembro nuevo = new Miembro(nombre, correo1, correo2, tipo);
+            this.miembros.Add(nuevo);
+        }
+
+        
+
+        public string miembrosToString()
+        {
+            string resultado = "";
+            for (int i = 0;i<this.miembros.Count;i++)
+            {
+                Miembro m = (Miembro) this.miembros[i];
+                m.toString();
+            }            
+            return resultado;
+        }
     }
 }
