793add49b4ff29f1109903e0c5013284333681f0
Fauricio <fauriciocr@gmail.com>
2017-10-10 16:10:50 -0600

    Actualización panel

--- Proyecto1/Proyecto1/Vista/Panel.Designer.cs
+++ Proyecto1/Proyecto1/Vista/Panel.Designer.cs
@@ -29,40 +29,39 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.CargaXls = new System.Windows.Forms.TabPage();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.openFileDialogXls = new System.Windows.Forms.OpenFileDialog();
             this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
+            this.CargaXls.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.CargaXls);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Location = new System.Drawing.Point(13, 13);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(500, 338);
+            this.tabControl1.Size = new System.Drawing.Size(766, 454);
             this.tabControl1.TabIndex = 0;
             // 
-            // tabPage1
+            // CargaXls
             // 
-            this.tabPage1.Controls.Add(this.button1);
-            this.tabPage1.Controls.Add(this.textBox1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(492, 312);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.CargaXls.Controls.Add(this.button1);
+            this.CargaXls.Controls.Add(this.textBox1);
+            this.CargaXls.Location = new System.Drawing.Point(4, 22);
+            this.CargaXls.Name = "CargaXls";
+            this.CargaXls.Padding = new System.Windows.Forms.Padding(3);
+            this.CargaXls.Size = new System.Drawing.Size(758, 428);
+            this.CargaXls.TabIndex = 0;
+            this.CargaXls.Text = "Previo";
+            this.CargaXls.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(68, 59);
+            this.button1.Location = new System.Drawing.Point(49, 43);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -83,26 +82,22 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(492, 312);
+            this.tabPage2.Size = new System.Drawing.Size(758, 428);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "consejo";
             this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // openFileDialogXls
-            // 
-            this.openFileDialogXls.FileName = "openFileDialogXls";
             // 
             // Panel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(525, 363);
+            this.ClientSize = new System.Drawing.Size(791, 479);
             this.Controls.Add(this.tabControl1);
             this.Name = "Panel";
-            this.Text = "Panel";
+            this.Text = "Administració consejo";
             this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
+            this.CargaXls.ResumeLayout(false);
+            this.CargaXls.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -110,10 +105,9 @@
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage CargaXls;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.OpenFileDialog openFileDialogXls;
     }
 }
