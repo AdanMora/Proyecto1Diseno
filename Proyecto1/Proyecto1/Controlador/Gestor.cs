95ef3d08e494425f3b3df9f0c0dec25f49534b44
Iván Calvo Pérez <ivanfelipecp@gmail.com>
2017-10-11 10:56:56 -0600

    + Cambios en gestor, le metí los controladores y atributos

--- Proyecto1/Proyecto1/Controlador/Gestor.cs
+++ Proyecto1/Proyecto1/Controlador/Gestor.cs
@@ -10,11 +10,25 @@ namespace Proyecto1.Controlador
     public class Gestor
     {
         private Consejo consejo;
-
+        private Controlador_Sesion controlador_sesion;
+        private Controlador_Solicitudes controlador_solicitudes;
+        
 
         public Gestor(Consejo consejo)
         {
             this.consejo = consejo;
+            this.setControladores();
+        }
+
+        public Gestor()
+        {
+            // this.consejo = load();
+            this.setControladores();
+        }
+
+        private void setControladores()
+        {
+            // Acá creamos/instanciamos los controladores
         }
 
 
