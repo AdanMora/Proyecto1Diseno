932dcc61f05c83f099fe2ff9415f46d6d3b6460e
Fauricio <fauriciocr@gmail.com>
2017-10-15 16:10:36 -0600

    Commit

--- Proyecto1/Proyecto1/Controlador/Strategy_DOCX.cs
+++ Proyecto1/Proyecto1/Controlador/Strategy_DOCX.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Proyecto1.Modelo;
+using System.IO;
 
 namespace Proyecto1.Controlador
 {
@@ -12,8 +13,32 @@ namespace Proyecto1.Controlador
         public void crearActa(Sesion puntos, string path)
         {
             // throw new NotImplementedException();
-        }
+            String line;
+            try
+            {
+                
+                StreamReader sr = new StreamReader(path+"Sample.txt");
 
+                //Read the first line of text
+                line = sr.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the lie to console window
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+                //close the file
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
         public void crearAgenda(object sesion, string path)
         {
             // Crea el DOCX
