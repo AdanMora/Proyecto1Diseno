81437cbb43efe3f69bc115f78c28c14058462d51
AdanMora <ajmorafallas@gmail.com>
2017-10-15 10:11:11 -0600

    GUI: almost finish

--- Proyecto1/Proyecto1/Vista/GUI.cs
+++ Proyecto1/Proyecto1/Vista/GUI.cs
@@ -56,5 +56,60 @@ namespace Proyecto1.Vista
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_MoverPunto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView4_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void clb_Asistencia_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_IniciarSesion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label27_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
